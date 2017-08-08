c6308ab2fcc45e96475818df2f763d9e3f03e59d
qdxxxx <qdx1992>
2017-08-08 09:35:51 +0800

    A indexBarLayout demo

---
+++ app/src/main/java/qdx/indexbarlayout/MainActivity.java
@@ -0,0 +1,80 @@
+package qdx.indexbarlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import qdx.indexbarlayout.datas.Car;
+import qdx.indexbarlayout.datas.CarsList;
+import qdx.indexbarlayout.datas.RecCarAdapter;
+import qdx.stickyheaderdecoration.NormalDecoration;
+
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.recView)
+    RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+        ButterKnife.bind(this);
+
+        final List<Car> carList = CarsList.getCars();
+
+        final RecCarAdapter adapter = new RecCarAdapter(this);
+        adapter.addDatas(carList);
+
+
+        final NormalDecoration decoration = new NormalDecoration() {
+            @Override
+            public String getHeaderName(int pos) {
+                return carList.get(pos).getHeaderName();
+            }
+        };
+
+        decoration.setOnHeaderClickListener(new NormalDecoration.OnHeaderClickListener() {
+            @Override
+            public void headerClick(int pos) {
+                Toast.makeText(MainActivity.this, "点击到头部" + carList.get(pos).getHeaderName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        final LinearLayoutManager manager = new LinearLayoutManager(this);
+        mRecyclerView.addItemDecoration(decoration);
+        mRecyclerView.setLayoutManager(manager);
+        mRecyclerView.setAdapter(adapter);
+
+
+        //侧边导航栏
+        IndexLayout indexLayout = (IndexLayout) findViewById(R.id.index_layout);
+        List<String> heads = new ArrayList<>();
+        for (Car car : carList) {
+            if (!heads.contains(car.getHeaderName())) {
+                heads.add(car.getHeaderName());
+            }
+        }
+        indexLayout.setIndexBarHeightRatio(0.9f);
+        indexLayout.getIndexBar().setIndexsList(heads);
+        indexLayout.getIndexBar().setIndexChangeListener(new IndexBar.indexChangeListener() {
+            @Override
+            public void indexChanged(String indexName) {
+                for (int i = 0; i < carList.size(); i++) {
+                    if (indexName.equals(carList.get(i).getHeaderName())) {
+                        manager.scrollToPositionWithOffset(i, 0);
+                        return;
+                    }
+                }
+            }
+        });
+    }
+
+}
